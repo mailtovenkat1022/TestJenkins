edd7dbc8818702f51c147d954818796da0df543c
Kajuluri <Kajuluri@SRIKAJULURI>
2017-07-23 12:49:06 -0400

    CAtModelAdd

---
+++ TestJenkins/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestJenkins.Models
+{
+    public class Category
+    {
+        public int CatID{ get; set; }
+        public string CatName { get; set; }
+    }
+}

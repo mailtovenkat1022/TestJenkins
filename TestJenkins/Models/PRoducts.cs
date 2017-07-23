5414a7c9a14f689f764f84207d4ac07594ee45be
Kajuluri <Kajuluri@SRIKAJULURI>
2017-07-23 12:43:26 -0400

    ProdctModelAdded

---
+++ TestJenkins/Models/PRoducts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestJenkins.Models
+{
+    public class PRoducts
+    {
+        public String ProductID { get; set; }
+        public String ProductName { get; set; }
+}
+}
